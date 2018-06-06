b2369941eecd8bcd90e17e9f59dc4039a6121fe8
Roman <roman.gorelik@gmail.com>
2018-06-06 23:29:10 +0300

    svd: revert files by name; no buffexplorer for Darwin yet

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git pull
 if [ `uname -s` = Darwin ]; then
     (( $IS_ROOT )) || VIMRC=~/.vimrc
     EXTENDED_REGEXP_KEY=E
+    VRC_EXCLUDE='jlanzarotta/bufexplorer'
 else
     if (( $IS_ROOT )); then
         VIMRC=/etc/vim/vimrc.local
@@ -13,9 +14,10 @@ else
         echo "ATTENTION! The screen escape is redefined to '^Jj' @/etc/screenrc"
     fi
     EXTENDED_REGEXP_KEY=r
+    VRC_EXCLUDE=XXXXXXX
 fi
 cat gred.src|sed "s/EXTENDED_REGEXP_KEY/$EXTENDED_REGEXP_KEY/" > gred
 (( $IS_ROOT )) && (DEST=/usr/local/bin; for a in v gred svd; do cp $a $DEST; chmod +x $DEST/$a; done)
 rm gred
-[ -n "$VIMRC" ] && cat vimrc > $VIMRC
+[ -n "$VIMRC" ] && cat vimrc|grep -v $VRC_EXCLUDE > $VIMRC
 v --up
