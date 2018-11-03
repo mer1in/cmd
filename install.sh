ffd89554027b6bae344cc3ba8acde4d322280c6c
Roman <roman.gorelik@gmail.com>
2018-11-03 21:18:39 +0200

    install as users aliases instead of system wide

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-let IS_ROOT=!`id -u`
 git pull
+VIMRC=~/.vimrc
 if [ `uname -s` = Darwin ]; then
-    (( $IS_ROOT )) || VIMRC=~/.vimrc
     EXTENDED_REGEXP_KEY=E
     VRC_EXCLUDE='jlanzarotta/bufexplorer'
 else
-    if (( $IS_ROOT )); then
-        VIMRC=/etc/vim/vimrc.local
-    fi
-    if [[ -f /etc/screenrc && -z "`cat /etc/screenrc|grep '^escape ^..'`" ]]; then
-        echo 'escape ^Jj' >> /etc/screenrc
-        echo "ATTENTION! The screen escape is redefined to '^Jj' @/etc/screenrc"
-    fi
     EXTENDED_REGEXP_KEY=r
     VRC_EXCLUDE=XXXXXXX
 fi
 cat gred.src|sed "s/EXTENDED_REGEXP_KEY/$EXTENDED_REGEXP_KEY/" > gred
-(( $IS_ROOT )) && (DEST=/usr/local/bin; for a in v gred svd; do cp $a $DEST; chmod +x $DEST/$a; done)
+mkdir -p ~/.v.utils 2>/dev/null
+for a in v gred svd cdr; do cp $a ~/.v.utils/; chmod +x ~/.v.utils/$a; done
 rm gred
-[ -n "$VIMRC" ] && cat vimrc|grep -v $VRC_EXCLUDE > $VIMRC
+cat vimrc|grep -v $VRC_EXCLUDE > $VIMRC
 v --up
+[ -f ~/.bashrc ] && sed -i '/#_V_UTILS_BEGIN_/,/#_V_UTILS_END_/d' ~/.bashrc
+cat <<EOM >>~/.bashrc
+#_V_UTILS_BEGIN_
+alias cdr='cd $(~/.v.utils/cdr)'
+alias gred='~/.v.utils/gred'
+alias svd='~/.v.utils/svd'
+alias v='~/.v.utils/v'
+#_V_UTILS_END_
+EOM
