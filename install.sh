b860c891073c945b533db8041f9a541e05fb857b
Roman <roman.gorelik@gmail.com>
2018-11-23 11:20:54 +0200

    fix install.sh @Darwin "sed: -i may not be used with stdin"

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@ VIMRC=~/.vimrc
 if [ `uname -s` = Darwin ]; then
     EXTENDED_REGEXP_KEY=E
     VRC_EXCLUDE='jlanzarotta/bufexplorer'
+    [ -f ~/.bashrc ] && sed -i '' '/#_V_UTILS_BEGIN_/,/#_V_UTILS_END_/d' ~/.bashrc
 else
     EXTENDED_REGEXP_KEY=r
     VRC_EXCLUDE=XXXXXXX
+    [ -f ~/.bashrc ] && sed -i '/#_V_UTILS_BEGIN_/,/#_V_UTILS_END_/d' ~/.bashrc
 fi
 cat gred.src|sed "s/EXTENDED_REGEXP_KEY/$EXTENDED_REGEXP_KEY/" > gred
 mkdir -p ~/.v.utils 2>/dev/null
 for a in v gred svd cdr; do cp $a ~/.v.utils/; chmod +x ~/.v.utils/$a; done
 rm gred
 cat vimrc|grep -v $VRC_EXCLUDE > $VIMRC
-[ -f ~/.bashrc ] && sed -i '/#_V_UTILS_BEGIN_/,/#_V_UTILS_END_/d' ~/.bashrc
 cat <<EOM >>~/.bashrc
 #_V_UTILS_BEGIN_
 alias cdr='cd \$(~/.v.utils/cdr)'
