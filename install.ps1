1cc9c7817bdeb010dcfdbe2ffa91c0dd702c985b
Roman <roman.gorelik@gmail.com>
2020-07-31 11:09:37 +0300

    debug: noexit

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
 (New-Object System.Net.WebClient).DownloadString('https://github.com/mer1in/v/raw/master/install.ps1') > $env:TEMP\vinstall.ps1
-Start-Process "$psHome\powershell.exe" -Verb Runas -ArgumentList "-Command", "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass ; $env:TEMP\vistall.ps1"
+Start-Process "$psHome\powershell.exe" -Verb Runas -ArgumentList "-NoExit", "-Command", "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass ; $env:TEMP\vistall.ps1"
