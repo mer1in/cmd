1fd8d45790e2930c32eee198e5ba4a72ef4cbb04
Roman <roman.gorelik@gmail.com>
2020-07-31 11:14:33 +0300

    typo

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
 (New-Object System.Net.WebClient).DownloadString('https://github.com/mer1in/v/raw/master/vinstall.ps1') > $env:TEMP\vinstall.ps1
-Start-Process "$psHome\powershell.exe" -ArgumentList "-NoExit", "-Command", "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass ; $env:TEMP\vistall.ps1"
+Start-Process "$psHome\powershell.exe" -ArgumentList "-NoExit", "-Command", "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass ; $env:TEMP\vinstall.ps1"
