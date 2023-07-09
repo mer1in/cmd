28c1808fa5b1bfd6b6946f2fcca359d34e799203
Roman Gorelik <romang@gilat.com>
2023-07-09 19:19:23 +0300

    install kubectl

--- install.sh
+++ install.sh
@@ -74,6 +74,16 @@ then
     do
         cat ./sync/ubuntu/$f > ~/$f
     done
+
+    kubectl version || {
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl
+        sudo apt-get install -y apt-transport-https
+        curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+        sudo apt-get update
+        sudo apt-get install -y kubectl
+    }
 fi
 ~/.v.utils/v --up
 
